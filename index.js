afd957755cc8b0999a1257304b93507cd3897a74
yanx <yanx@oupeng.com>
2015-07-22 11:44:39 +0800

    add comments

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+/**
+ * @file 发送html中js与css并替换的gulp插件
+ */
+
 "use strict";
 
 var through = require('through2');
@@ -9,9 +13,14 @@ var q = require('q');
 var _ = require('lodash');
 
 var PLUGIN_NAME = 'gulp-x-deploy';
+//将代码中 <!--debug start--> code here <!--debug end--> 标识的测试代码删掉
 var debugReg = /[ \f\t\v]*<!--\s*debug\s+start\s*-->[\s\S]*?<!--\s*debug\s+end\s*-->[ \f\t\v]*/ig;
 
-
+/**
+ * @desc 发送html中js与css并替换
+ * @param opts
+ * @returns {*}
+ */
 module.exports = function index(opts) {
 
   return through.obj(function (file, encoding, cb) {
@@ -24,15 +33,18 @@ module.exports = function index(opts) {
     }
 
     var ext = path.extname(file.path);
+    //只支持html后缀的文件
     if(ext != '.html') {
       return cb(new gutil.PluginError(PLUGIN_NAME, 'Not support file: ' + file.path + ' !'))
     }
     var newHtml = file.contents.toString();
     newHtml = newHtml.replace(debugReg, '');
     q.fcall(function() {
+      //扫出需替换的js与css路径
       return scanhtml(file.contents.toString(), opts)
     })
     .then(function(output) {
+        //遍历路径发送上线
         return q.all(_.flattenDeep([output['js'].map(function(js) {
           return upload(file, js);
         }), output['css'].map(function(css) {
@@ -40,6 +52,7 @@ module.exports = function index(opts) {
         })]));
       })
     .then(function(result) {
+        //替换
         result.forEach(function(item) {
           if(item != null) {
             newHtml = newHtml.replace(item.str, (item.str.replace(item.rel, item.cdn)));
@@ -54,7 +67,7 @@ module.exports = function index(opts) {
       }.bind(this))
     .catch(function(msg) {
         debug.error(msg);
-        cb(msg);
+        cb(new gutil.PluginError(PLUGIN_NAME, msg));
       });
   })
 }
