ad09288a88e61179da39ec3656aeeddc265902a5
yanx <yanx@oupeng.com>
2015-09-20 19:51:20 +0800

    add replace host

--- test.js
+++ test.js
@@ -4,5 +4,7 @@ var deploy = require('./');
 var gulp = require('gulp');
 
 gulp.src('./test/1.html')
-  .pipe(deploy())
+  .pipe(deploy({
+    host: 'baidu.com'
+  }))
   .pipe(gulp.dest('./test/dist'));
