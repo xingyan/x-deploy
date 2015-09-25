eaeb97130cc411387871428c8b248337c029cf77
zhuyingda <383329144@qq.com>
2015-09-25 18:36:26 +0800

    add param min

--- test.js
+++ test.js
@@ -6,9 +6,10 @@ var gulp = require('gulp');
 gulp.src('./test/1.html')
   .pipe(deploy({
     host: {
-      max: 8,
-      name: 'baidu.com',
-      prefix: 'i'
+      max: 10,
+      min: 5,
+      name: 'yx-s.com',
+      prefix: 'p'
     }
   }))
   .pipe(gulp.dest('./test/dist'));
